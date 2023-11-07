74888f4624cf86912ba47eb100c13a0b29bffaec
Andrew Crosby <awcrosby@gmail.com>
2023-11-06 20:41:32 -0500

    Take github commit info and send to chatgpt for summary
    Get commit message, related pr info, and code patches
    from github for one or more commits.
    
    Send all commit info or grouping of data about multiple commits
    to chatgpt for summary of the change.

---
+++ api_calls.py
@@ -0,0 +1,88 @@
+from dotenv import load_dotenv
+import json
+import requests
+import os
+
+load_dotenv()
+GITHUB_API_KEY = os.environ.get("GITHUB_API_KEY")
+OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY")
+
+SUMMARY_PROMPT_INTRO = """
+I need a summary of code changes made to a git repository.
+
+Ignore issue tracker issues and do not include or mention associated issues in the summary.
+
+Summarize all the changes together in a single paragraph not in a list.
+Respond with a maximum total of 4 sentences.
+
+Please focus on the types of changes.
+Do not focus on the the names of the files, do not focus on the number of lines edited.
+"""
+
+def get_changes_single_commit(owner, repo, commit_sha):
+    gh_commit_url = f"https://api.github.com/repos/{owner}/{repo}/commits/{commit_sha}"
+    headers = {"Authorization": f"token {GITHUB_API_KEY}"}
+    commit_data = requests.get(gh_commit_url, headers=headers).json()
+
+    commit_message = commit_data["commit"]["message"]
+
+    FILE_KEYS = ["filename", "patch", "status", "changes"]
+    patches = []
+    for file in commit_data["files"]:
+        d = {k: v for k, v in file.items() if k in FILE_KEYS}
+        patches.append(d)
+
+    pulls_url = gh_commit_url + "/pulls"
+    pull_data = requests.get(pulls_url, headers=headers).json()
+    PULL_REQUEST_KEYS = ["title", "body"]
+    pull_requests = []
+    for pull in pull_data:
+        d = {k: v for k, v in pull.items() if k in PULL_REQUEST_KEYS}
+        pull_requests.append(d)
+    
+    return (commit_message, patches, pull_requests)
+
+
+def call_openai(content):
+    openai_url = "https://api.openai.com/v1/chat/completions"
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": f"Bearer {OPENAI_API_KEY}",
+    }
+    payload = {"model": "gpt-3.5-turbo", "messages": [{"role": "user", "content": content}]}
+    data = json.dumps(payload)
+
+    token_estimate = int(len(content) / 4)
+    if token_estimate > 4097:
+        raise RuntimeError(f"Token estimate {token_estimate} exceeds maximum 4097 tokens for OpenAI API")
+
+    r = requests.post(openai_url, headers=headers, data=data)
+    ai_reply = r.json()['choices'][0]['message']['content']
+    return ai_reply
+
+
+def ai_summarize_commit(commit_message, patches, pull_requests):
+    prompt = SUMMARY_PROMPT_INTRO + f"""
+
+    Pull request text: {pull_requests}
+
+    Commit message: {commit_message}
+
+    Code patches: {patches}
+    """
+
+    return call_openai(content=prompt)
+
+
+def ai_summarize_single_data_type(data_type, changes):
+    PROMPT_TEMPLATE = SUMMARY_PROMPT_INTRO + """
+    Here are the {data_type} items I want you to summarize: {list_of_code_changes}
+    """
+
+    code_changes_str = ""
+    for i, d in enumerate(changes):
+        code_changes_str += d + "\n\n\n"
+
+    prompt = PROMPT_TEMPLATE.format(data_type=data_type, list_of_code_changes=code_changes_str)
+
+    return call_openai(content=prompt)
---
+++ app.py
@@ -0,0 +1,58 @@
+import enum
+from pprint import pprint
+
+from api_calls import ai_summarize_commit, ai_summarize_single_data_type, get_changes_single_commit
+
+
+class CodeChangeType(enum.Enum):
+    """Descriptions of change type for use in prompts."""
+    COMMIT_MESSAGE = "git commit message text"
+    PULL_REQUEST = "pull request text"
+    CODE_PATCH = "code edits in the form of a code diff patch"
+
+
+### Github info input
+owner = "awcrosby"
+repo = "galaxy-importer"
+commit_sha = "573a5994fcf534fafe27372a6fe098fb7bb14c62"
+
+# commit_message, patches, pull_requests = get_changes_single_commit(owner, repo, commit_sha)
+# single_commit_summary = ai_summarize_commit(commit_message, patches, pull_requests)
+# pprint(single_commit_summary)
+
+list_of_commits = [
+    "eb5ead417c703363beb04d082eb3b2a0767af6fb",
+    "16c76a32839c08e1faaaa18383a4ca2328282b34",
+    "475c6a00d5788b6a2e3486f10627f5967dc2f826",
+    "968e0db27be862b5fac9a3e49b0f5ecf62213164",
+    "05712d1d76e934f5b5952bf8cf2ac29aa310a0e8",
+    "573a5994fcf534fafe27372a6fe098fb7bb14c62",
+]
+
+
+def get_changes_multiple_commits(owner, repo, list_of_commits):
+    code_changes = []
+    for commit_sha in list_of_commits:
+        code_change = get_changes_single_commit(owner, repo, commit_sha)
+        code_changes.append(code_change)
+    return code_changes
+
+code_changes = get_changes_multiple_commits(owner, repo, list_of_commits)
+all_commit_messages = [c[0] for c in code_changes]
+all_pull_requests = [c[2] for c in code_changes]
+all_patches = [c[1] for c in code_changes]
+
+commit_titles = [m.split("\n")[0] for m in all_commit_messages]
+pprint(f"========= Summarizing the following commits =========\n{commit_titles}")
+
+resp = ai_summarize_single_data_type(CodeChangeType.COMMIT_MESSAGE, changes=all_commit_messages)
+pprint("========= Summary of git commit message text =========")
+pprint(resp)
+
+resp = ai_summarize_single_data_type(CodeChangeType.PULL_REQUEST, changes=all_pull_requests)
+pprint("========= Summary of pull request text =========")
+pprint(resp)
+
+resp = ai_summarize_single_data_type(CodeChangeType.CODE_PATCH, changes=all_patches)
+pprint("========= Summary of code code diff patches =========")
+pprint(resp)
